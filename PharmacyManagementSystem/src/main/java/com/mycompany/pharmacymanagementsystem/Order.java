4017bec94c8bbc7e5c61e91fde814a670ba4f1c5
Abobakr Mostafa Sayed Abdulrahman <abobakr.abdelrahman45@gmail.com>
2025-05-13 22:40:59 +0300

    Add Order and Payment classes

--- PharmacyManagementSystem/src/main/java/com/mycompany/pharmacymanagementsystem/Order.java
+++ PharmacyManagementSystem/src/main/java/com/mycompany/pharmacymanagementsystem/Order.java
@@ -4,10 +4,52 @@
  */
 package com.mycompany.pharmacymanagementsystem;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  *
  * @author alima
  */
 public class Order {
-    
+    private int orderID;
+    private Cashier cashier;
+    private List<PharmacyItem> items;
+    private LocalDate orderDate;
+    private double totalAmount;
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public Cashier getCashier() {
+        return cashier;
+    }
+
+    public List<PharmacyItem> getItems() {
+        return items;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void calculateTotalAmount() {
+        totalAmount = 0;
+        for (PharmacyItem item : items) {
+            totalAmount += item.getPrice(); // assuming getPrice() in item class
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Order ID: " + orderID +
+                "\nCashier: " + cashier +
+                "\nOrder Date: " + orderDate +
+                "\nTotal Amount: " + totalAmount;
+    }
 }
--- PharmacyManagementSystem/src/main/java/com/mycompany/pharmacymanagementsystem/Payment.java
+++ PharmacyManagementSystem/src/main/java/com/mycompany/pharmacymanagementsystem/Payment.java
@@ -4,10 +4,52 @@
  */
 package com.mycompany.pharmacymanagementsystem;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author alima
  */
 public class Payment {
-    
+    private int paymentID;
+    private Order order;
+    private LocalDate paymentDate;
+    private double amountPaid;
+    private String paymentMethod;
+
+    public int getPaymentID() {
+        return paymentID;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public double getAmountPaid() {
+        return amountPaid;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public boolean processPayment() {
+        if (order != null && amountPaid >= order.getTotalAmount()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment ID: " + paymentID +
+                "\nOrder ID: " + (order != null ? order.getOrderID() : "None") +
+                "\nPayment Date: " + paymentDate +
+                "\nAmount Paid: " + amountPaid +
+                "\nPayment Method: " + paymentMethod;
+    }
 }
